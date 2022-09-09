f025cfc6ad744e2716a2062769604e58cd95eb09
Prashanth Puranik <puranik@digdeeper.in>
2022-09-09 18:07:34 +0530

    Using a router-link to prevent page refresh

--- workshops-app/src/components/AppMenu.vue
+++ workshops-app/src/components/AppMenu.vue
@@ -1,19 +1,26 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="/">WorkshopsApp</b-navbar-brand>
+    <b-navbar toggleable="lg" type="light" variant="light">
+      <b-navbar-brand to="/">
+        WorkshopsApp
+      </b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="/workshops">List of workshops</b-nav-item>
-          <b-nav-item href="/workshops/add">Add a workshop</b-nav-item>
+            <!-- if we use to prop, b-nav-item will internally a vue-router component called router-link which takes in a "to" prop -->
+          <b-nav-item to="/workshops">
+            List of workshops
+          </b-nav-item>
+          <b-nav-item to="/workshops/add">
+            Add a workshop
+          </b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-            <b-nav-item href="/logout">Logout</b-nav-item>
+          <b-nav-item to="/logout">Logout</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -22,7 +29,7 @@
 
 <script>
 export default {
-    name: 'AppMenu'
+  name: "AppMenu",
 };
 </script>
 
